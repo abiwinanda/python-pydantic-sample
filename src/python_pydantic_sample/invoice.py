5cee0ad179f70934cc374d25a4f7e25e5f47cf2d
abiwinanda <abiwinanda@outlook.com>
2025-03-13 17:44:53 +0200

    add internal computed field

--- src/python_pydantic_sample/invoice.py
+++ src/python_pydantic_sample/invoice.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import Optional
 from .user import User
 from decimal import Decimal
-from pydantic import BaseModel, EmailStr, field_validator, model_validator
+from pydantic import BaseModel, EmailStr, model_validator
 
 class InvoiceStatus(str, Enum): 
     DRAFT = "draft"
@@ -13,6 +13,7 @@ class Invoice(BaseModel):
     description: str
     customer: Optional[User]
     amount: Decimal
+    tax: Decimal
     status: InvoiceStatus
 
     # This method runs before the model is created
@@ -24,4 +25,15 @@ class Invoice(BaseModel):
         if status != "draft" and customer is None:
             raise ValueError('non-draft invoice must have a customer')
         
+        return values
+
+    @model_validator(mode='before')
+    def calculate_tax(cls, values):
+        try:
+            amount = Decimal(values.get('amount'))
+        except:
+            raise ValueError("amount is not a valid decimal")
+
+        values['tax'] = Decimal("0.1") * amount
+
         return values
